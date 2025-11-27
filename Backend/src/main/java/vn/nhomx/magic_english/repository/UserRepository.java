0ae7eb84cc7e7b4dc0c3bd243b75e0f20e96d8cc
Min-Duc1705 <146452065+Min-Duc1705@users.noreply.github.com>
2025-11-27 20:00:00 +0700

    [BE] feat(user): create file UserController and add api CRUD

--- Backend/src/main/java/vn/nhomx/magic_english/repository/UserRepository.java
+++ Backend/src/main/java/vn/nhomx/magic_english/repository/UserRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import vn.nhomx.magic_english.model.User;
@@ -13,5 +11,9 @@ import vn.nhomx.magic_english.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-   
+    User findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    User findByRefreshTokenAndEmail(String token, String email);
 }
