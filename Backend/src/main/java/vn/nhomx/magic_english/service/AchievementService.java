fbc9fbd8053bdad287d39fdeb26a72489d120ecc
Min-Duc1705 <leminhduc170504@gmail.com>
2025-12-02 20:30:00 +0700

    [BE] feat(achievement): create file AchievementService

---
+++ Backend/src/main/java/vn/nhomx/magic_english/service/AchievementService.java
@@ -0,0 +1,15 @@
+package vn.nhomx.magic_english.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.nhomx.magic_english.model.Achievement;
+import vn.nhomx.magic_english.repository.AchievementRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AchievementService {
+    
+
+}
