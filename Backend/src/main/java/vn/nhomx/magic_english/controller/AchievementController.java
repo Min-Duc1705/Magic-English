ee64b29c3920a85cffe9d709bdb6a2a7cb880f4a
Min-Duc1705 <leminhduc170504@gmail.com>
2025-12-02 20:30:00 +0700

    [BE] feat(achievement): create file AchievementController

---
+++ Backend/src/main/java/vn/nhomx/magic_english/controller/AchievementController.java
@@ -0,0 +1,16 @@
+package vn.nhomx.magic_english.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vn.nhomx.magic_english.model.Achievement;
+import vn.nhomx.magic_english.service.AchievementService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/achievements")
+public class AchievementController {
+
+    
+}
