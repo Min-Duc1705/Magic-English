26ca185974cd16d277c18288d6f8e192fed1c6ce
Min-Duc1705 <leminhduc170504@gmail.com>
2025-12-05 20:30:00 +0700

    [BE] feat(file): create file FileService

---
+++ Backend/src/main/java/vn/nhomx/magic_english/service/FileService.java
@@ -0,0 +1,25 @@
+package vn.nhomx.magic_english.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+
+    
+}
