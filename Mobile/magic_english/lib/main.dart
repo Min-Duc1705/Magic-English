6607eb7ad322c8c4fbd60feb0a145a41341c2558
Min-Duc1705 <leminhduc170504@gmail.com>
2025-12-07 20:30:00 +0700

    [MB] feat(vocabulary): declaration VocabularyProvider on main.dart

--- Mobile/magic_english/lib/main.dart
+++ Mobile/magic_english/lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:magic_enlish/features/vocabulary/vocabulary_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:magic_enlish/providers/vocabulary/vocabulary_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => VocabularyProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        locale: const Locale('en', 'US'),
+        home: const VocabularyScreen(),
+        
       ),
-      home: const VocabularyScreen(),
     );
   }
 }
-
-
