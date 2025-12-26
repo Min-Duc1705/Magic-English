dfc01003336527ae31447364ef1bf7e7129e48dd
huyen64ktpm1 <thanhhuyentk2k4@icloud.com>
2025-12-26 23:30:00 +0700

    [MB] fix(auth): fix error label input

--- Mobile/magic_english/lib/providers/auth/auth_provider.dart
+++ Mobile/magic_english/lib/providers/auth/auth_provider.dart
@@ -24,7 +24,7 @@ class AuthProvider with ChangeNotifier {
       await prefs.setString('refresh_token', user.refreshToken!);
     }
 
-    // chỉ persist avatar khi có giá trị thuc2
+    // chỉ persist avatar khi có giá trị thuc cua 
     if (user.avatarUrl != null && user.avatarUrl!.isNotEmpty) {
       await prefs.setString('avatarUrl', user.avatarUrl!);
     } else {
