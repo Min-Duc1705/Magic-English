d4df8cd68c114a18fa5d142b42e6b40494f2486c
huyen64ktpm1 <thanhhuyentk2k4@icloud.com>
2025-12-26 23:30:00 +0700

    [MB] fix(auth): fix error label input name

--- Mobile/magic_english/lib/providers/auth/auth_provider.dart
+++ Mobile/magic_english/lib/providers/auth/auth_provider.dart
@@ -35,7 +35,7 @@ class AuthProvider with ChangeNotifier {
     debugPrint('AUTH_PROVIDER: setUser avatarUrl=${user.avatarUrl}');
   }
 
-  // Load thong tin user từ  SharedPreferences khi mở app len 
+  // Load 1thong tin user từ  SharedPreferences khi mở app len 
   Future<void> loadUser() async {
     print('🔐 ========== LOADING USER FROM STORAGE ==========');
     final prefs = await SharedPreferences.getInstance();
