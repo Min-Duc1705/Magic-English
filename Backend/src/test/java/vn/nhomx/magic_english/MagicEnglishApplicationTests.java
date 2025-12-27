fd34eecf082184104f85aa9a3366023a434785c0
Min-Duc1705 <146452065+Min-Duc1705@users.noreply.github.com>
2025-12-27 20:05:00 +0700

    [BE] chore: initialize project structure

---
+++ Backend/src/test/java/vn/nhomx/magic_english/MagicEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nhomx.magic_english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicEnglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
